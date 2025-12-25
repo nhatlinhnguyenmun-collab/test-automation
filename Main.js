ad3e1229a5dcc7b7d463b3f85f402cea2e228d8c
Linh <linh@gmail.com>
2025-12-26 00:01:19 +0700

    update Main.js

---
+++ Main.js
@@ -0,0 +1,18 @@
+console.log("Hello");
+console.log("Hi");
+var a = 10; 
+console.log("conflict test hien branch");
+
+
+
+console.log("Hello, VS Code");
+console.log("Sum:", 2 + 3);
+// 
+// * git checkout mai ->  Add code -> git add -> git commit 
+// * git checkout hien ->  Add code -> git add -> git commit 
+// * git checkout main ->  Git merge mai -> git add -> git commit 
+// * git merge hien -> conflict -> handle conflict -> git add -> git commit 
+
+// Git remote -> github vs gitlab  
+
+console.log("Code đang chạy rồi");
